2eab411ac60414343ad3281449f29146d6efdddc
Nathan Bienvenu <bienvenu.nathan72@gmail.com>
2024-02-12 22:16:48 +0100

    Initial Commit

---
+++ Blacklist it/Blacklist_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blacklist_itApp.swift
+//  Blacklist it
+//
+//  Created by NWX72-XXI on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Blacklist_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
